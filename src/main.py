2396bdc5a7d3a069b8dd6ed16976be6b7ce0c684
lam <huylamho6@gmail.com>
2023-08-27 00:43:33 +0700

    commit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"My Github: https://github.com/Laamhohuy"}
+
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
